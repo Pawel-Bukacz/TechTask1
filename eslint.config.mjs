7f00d47e76ad5839079012705ca40d6a8a297d19
Your Name <you@example.com>
2025-08-15 16:35:26 +0200

    chore: installed eslint, connected it with prettier and playwright. installed husky. installed trivago plugin. Updated framework config.

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import pluginJs from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+// import eslintPluginPlaywright from 'eslint-plugin-playwright';
+
+export default [
+  {
+    ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**'],
+  },
+  { files: ['**/*.ts'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'no-console': 'error',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+  },
+  {
+    rules: {
+      'playwright/no-nested-step': 'off',
+    },
+    settings: {
+      playwright: {
+        globalAliases: {
+          test: ['setup'],
+        },
+      },
+    },
+  },
+  eslintPluginPrettierRecommended,
+];
